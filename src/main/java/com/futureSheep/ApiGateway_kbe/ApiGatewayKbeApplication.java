53b6a66db0923e2146d8587b7781067ecc377e8c
s-fx <fxscholler@protonmail.com>
2022-01-05 17:48:57 +0100

    init project

---
+++ src/main/java/com/futureSheep/ApiGateway_kbe/ApiGatewayKbeApplication.java
@@ -0,0 +1,13 @@
+package com.futureSheep.ApiGateway_kbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayKbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayKbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futureSheep/ApiGateway_kbe/ApiGatewayKbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futureSheep.ApiGateway_kbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayKbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
